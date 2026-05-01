d86a7d295b371597ae90ddd62b7e4fb93a89f728
d00286767 <d00286767@student.dkit.ie>
2026-05-01 23:32:31 +0100

    Add file and directory management options to script

--- finallab.sh
+++ finallab.sh
@@ -30,6 +30,55 @@ do
    echo "Disk usage: $(disk)%"
    echo "Memory usage: $(mem)%"
    echo "Top process: $(pro)%";;
+ 
+
+
+   echo "----- File Management -----"
+   echo "1. Create File"
+   echo "2. Create Directory"
+   echo "3. Backup Directory"
+   echo "4. Back to Main Menu"
+   read -p "Choose option: " choice
+
+   case $choice in
+    1)
+       read -p "Enter file name: " file
+       if [ -e "$file" ];
+       then
+          echo "File already exists!"
+       else
+            touch "$file"
+            echo "File created."
+        fi;;
+
+     2)
+        read -p "Enter directory name: " dir
+        if [ -d "$dir" ]; then
+               echo "Directory already exists!"
+         else
+              mkdir "$dir"
+              echo "Directory created."
+         fi ;;
+
+      3)  read -p "Enter source directory: " src
+           read -p "Enter backup location: " dest
+
+           if [ ! -d "$src" ]; then
+                echo "Source directory does not exist!"
+           else
+                 mkdir -p "$dest"
+                 cp -r "$src" "$dest"
+                 echo "Backup completed."
+           fi;;
+
+       4)break;;
+
+       *)
+                echo "Invalid option!"
+                ;;
+        esac
+
+        echo
    
  3)echo "";;
  4)echo "";;
